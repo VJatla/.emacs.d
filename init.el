4b6802c2fcf602443116d8833793552ecca633c1
Venkatesh Jatla <venkatesh.jatla@gmail.com>
2024-08-31 20:22:06 -0700

    Switching to new org file.

--- init.el
+++ init.el
@@ -1,27 +1,54 @@
-;; Loading custom file with automatically generated settings
-(setq custom-file "~/.emacs.d/auto-gen-custom.el")
+
+;; Automatically generated customization goes to another file
+
+(setq custom-file "~/.emacs.d/auto-customization.el")
 (load custom-file)
 
-;; Disable package.el in favor of straight.el
-(setq package-enable-at-startup nil)
 
-;; Installing required packages
-(org-babel-load-file "~/.emacs.d/install_packages.org")
-(org-babel-load-file "~/.emacs.d/looks.org")
-(org-babel-load-file "~/.emacs.d/function.org")
-(org-babel-load-file "~/.emacs.d/agenda.org")
-(org-babel-load-file "~/.emacs.d/org.org")
+;; Emacs package management, use-package + quelpa
+
+(require 'package)
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t);; (unofficial) Milkypostman’s Emacs Lisp Package Archive
+(package-initialize) 
+
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+(eval-and-compile
+  (setq use-package-always-ensure t
+        use-package-expand-minimally t))
+
+
+;; quelpa
+;; If you cannot install automatically then
+;;     (1) Download quelpa.el from https://github.com/quelpa/quelpa/blob/master/quelpa.el
+;;     (2) place in a directory having load-path
+;;     (3) Add this line ---> (add-to-list 'load-path "~/.emacs.d/manually-downloaded/")
+(unless (package-installed-p 'quelpa)
+  (with-temp-buffer
+    (url-insert-file-contents "https://raw.githubusercontent.com/quelpa/quelpa/master/quelpa.el")
+    (eval-buffer)
+    (quelpa-self-upgrade)))
+(require 'quelpa)
+
+;; quelpa with use-package
+(quelpa
+ '(quelpa-use-package
+   :fetcher git
+   :url "https://github.com/quelpa/quelpa-use-package.git"))
+(require 'quelpa-use-package)
+
+;; Nice defaults
+(org-babel-load-file "~/.emacs.d/defaults.org")
 (org-babel-load-file "~/.emacs.d/markdown.org")
-;;(org-babel-load-file "~/.emacs.d/denote.org")
-(org-babel-load-file "~/.emacs.d/latex.org")
-(org-babel-load-file "~/.emacs.d/configuration.org")
+(org-babel-load-file "~/.emacs.d/agenda.org")
+(org-babel-load-file "~/.emacs.d/denote.org")
+(org-babel-load-file "~/.emacs.d/dired.org")
+(org-babel-load-file "~/.emacs.d/csv.org")
+(org-babel-load-file "~/.emacs.d/project-management.org")
+(org-babel-load-file "~/.emacs.d/completion-framework.org")
+(org-babel-load-file "~/.emacs.d/checking-framework.org")
+(org-babel-load-file "~/.emacs.d/lsp.org")
+(org-babel-load-file "~/.emacs.d/python.org")
+(org-babel-load-file "~/.emacs.d/snippets.org")
 (org-babel-load-file "~/Dropbox/keybindings.org")
-
-;; Setting up cursor colors after loading everything
-(setq ryo-modal-cursor-type 'box)
-(setq ryo-modal-cursor-color "#CE0058")  ;; Land Grant Grey (VT seondary color)
-
-(setq-default cursor-type 'bar)
-(setq ryo-modal-default-cursor-color "#D7D2CB") ;; Boundless Pink (VT secondary color)
-(set-cursor-color "#D7D22B")
-
